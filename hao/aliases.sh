ceff31f4e239efeddb5442e931bdfe85d2d0be59
Katie Hoffman <katie@Katies-MacBook-Pro.local>
2019-07-29 15:23:22 -0400

    aliases

--- hao/aliases.sh
+++ hao/aliases.sh
@@ -59,6 +59,14 @@ alias up="git fetch --all && git fetch -v origin master:master && git remote -v 
 alias uph="git fetch --all && git fetch -v origin master:master hatchery:hatchery && git remote -v prune origin"
 alias show="git show"
 
+alias util='kubectl get nodes --no-headers | awk '\''{print $1}'\'' | xargs -I {} sh -c '\''echo {} ; kubectl describe node {} | grep Allocated -A 5 | grep -ve Event -ve Allocated -ve percent -ve -- ; echo '\'''
+
+# Get CPU request total (we x20 because because each m3.large has 2 vcpus (2000m) )
+alias cpualloc='util | grep % | awk '\''{print $1}'\'' | awk '\''{ sum += $1 } END { if (NR > 0) { print sum/(NR*20), "%\n" } }'\'''
+
+# Get mem request total (we x75 because because each m3.large has 7.5G ram )
+alias memalloc='util | grep % | awk '\''{print $5}'\'' | awk '\''{ sum += $1 } END { if (NR > 0) { print sum/(NR*75), "%\n" } }'\'''
+
 hash -d w=~/w/dashboard
 hash -d work=~/w
 hash -d lab=~/lab
